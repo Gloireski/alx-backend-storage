a36b2943a740f9ff47a7407d89b6c9a06688f0f0
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-18 01:44:58 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,9 @@
 redis
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
+Utype = Union[str, bytes, int, float]
 
 
 class Cache:
@@ -21,3 +22,21 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+
+def get(key: str, fn: Optional[Callable] = None) -> Utype:
+    """ def get """
+    if fn:
+        return fn(self._redis.get(key))
+    data = self._redis.get(key)
+    return data
+
+
+def get_int(self: bytes) -> int:
+    """get a number"""
+    return int.from_bytes(self, sys.byteorder)
+
+
+def get_str(self: bytes) -> str:
+    """get a string"""
+    return self.decode("utf-8")
