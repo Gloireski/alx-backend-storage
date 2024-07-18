e66eccd78c502aa017fde509b724e05465f30993
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-18 22:57:40 +0000

    fix task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,51 +1,55 @@
 #!/usr/bin/env python3
-""" Redis client module
+"""
+redis
 """
 import redis
+from typing import Union, Callable, Optional, Any
 from uuid import uuid4
-from functools import wraps
-from typing import Any, Callable, Optional, Union
+Utype = Union[str, bytes, int, float]
 
 
 class Cache:
-    """ Caching class
     """
-    def __init__(self) -> None:
-        """ Initialize new cache object
-        """
+    define class Cache
+    """
+    def __init__(self):
+        """ constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes,  int,  float]) -> str:
-        """ Stores data in redis with randomly generated key
-        """
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ def store """
         key = str(uuid4())
-        client = self._redis
-        client.set(key, data)
+        self._redis.mset({key: data})
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
-        """ Gets key's value from redis and converts
-            result byte  into correct data type
-        """
-        client = self._redis
-        value = client.get(key)
-        if not value:
-            return
-        if fn is int:
-            return self.get_int(value)
-        if fn is str:
-            return self.get_str(value)
-        if callable(fn):
-            return fn(value)
-        return value
 
-    def get_str(self, data: bytes) -> str:
-        """ Converts bytes to string
-        """
-        return data.decode('utf-8')
+def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+    """
+    convert the data back
+    to the desired format
+    :param key:
+    :param fn:
+    :return:
+    """
+    client = self._redis
+    value = client.get(key)
+    if not value:
+        return
+    if fn is int:
+        return self.get_int(value)
+    if fn is str:
+        return self.get_str(value)
+    if callable(fn):
+        return fn(value)
+    return value
 
-    def get_int(self, data: bytes) -> int:
-        """ Converts bytes to integers
-        """
-        return int(data)
+
+def get_int(self: bytes) -> int:
+    """get a number"""
+    return int.from_bytes(self, sys.byteorder)
+
+
+def get_str(self: bytes) -> str:
+    """get a string"""
+    return self.decode("utf-8")
