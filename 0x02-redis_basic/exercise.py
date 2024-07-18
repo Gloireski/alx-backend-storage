4f803492f1cb2b615f8fdf74aab5dfd423a70b93
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-18 01:22:41 +0000

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+redis
+"""
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Cache:
+    """
+    define class Cache
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ def store """
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
